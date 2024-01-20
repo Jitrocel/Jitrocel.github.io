d3902ebdcc37722e705d45f444d1eb45a2c962ce
Jitrocel <156829820+Jitrocel@users.noreply.github.com>
2024-01-20 01:22:19 +0100

    Create loadGEOJSON.js

---
+++ loadGEOJSON.js
@@ -0,0 +1,51 @@
+// Načtení GeoJSON souboru (předpokládá se, že je umístěn ve stejném adresáři jako HTML soubor)
+    function loadGEOJSON(route){
+    fetch(route)
+        .then(response => response.json())
+        .then(data => {
+            // Funkce pro vložení dat do tabulky
+            function populateTable(features) {
+                //const tableBody = document.getElementById('geoTableBody');
+
+                features.forEach(feature => {
+                    const geometry = feature.geometry;
+                    const coordinates = geometry.coordinates;
+
+                    //const row = document.createElement('tr');
+                    //const idCell = document.createElement('td');
+                    //const nameCell = document.createElement('td');
+                    //const latCell = document.createElement('td');
+                    //const lonCell = document.createElement('td');
+
+
+                    let jmeno = [];
+                    let x = [];
+                    let y = [];
+
+                    nazev = (feature.properties.nazev);
+                    x = (coordinates[1]);
+                    y = (coordinates[0]);
+                    alert(nazev + " ; " + x + " ; " + y);
+                    //idCell.textContent = feature.id;
+                    //nameCell.textContent = feature.properties.name; // Přidání jména (přizpůsobte podle skutečného názvu vlastnosti)
+                    //latCell.textContent = coordinates[1];
+                    //lonCell.textContent = coordinates[0];
+
+                    //row.appendChild(idCell);
+                    //row.appendChild(nameCell);
+                    //row.appendChild(latCell);
+                    //row.appendChild(lonCell);
+
+                    //tableBody.appendChild(row);
+                });
+            }
+
+            // Získání seznamu prvků (bodů) z GeoJSON dat
+            const features = data.features;
+
+            // Volání funkce pro vložení dat do tabulky
+            populateTable(features);
+        })
+        .catch(error => console.error('Error loading GeoJSON:', error));
+    }
+    
